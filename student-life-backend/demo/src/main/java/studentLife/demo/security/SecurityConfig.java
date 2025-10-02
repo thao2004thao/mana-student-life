a41cdfdb0f3ca93505f2d565569431d7a5bb796f
hungnn <hungnguyenngoc714@gmail.com>
2025-10-02 10:08:49 +0700

    update port fe

--- student-life-backend/demo/src/main/java/studentLife/demo/security/SecurityConfig.java
+++ student-life-backend/demo/src/main/java/studentLife/demo/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
+                        .requestMatchers("/api/users/register", "/api/users/login","/api/users/refresh").permitAll()
                         .requestMatchers("/api/courses/**").authenticated()
                         .requestMatchers("/api/expenses/**").authenticated()
                         .requestMatchers("/api/tasks/**").authenticated()
